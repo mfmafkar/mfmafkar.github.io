0fa085852bb09874d6c2f99dd6e45d37b6b6d316
mfmafkar <35808593+mfmafkar@users.noreply.github.com>
2024-10-06 22:52:01 +0530

    feat: Added mailto option to portofolio

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -2,6 +2,7 @@ import { Box, IconButton } from '@mui/material';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import TwitterIcon from '@mui/icons-material/Twitter';
+import EmailIcon from '@mui/icons-material/Email';
 
 const SocialLinks = () => {
   return (
@@ -29,8 +30,12 @@ const SocialLinks = () => {
         >
           <TwitterIcon />
         </IconButton>
+        <IconButton aria-label="Email" href="mailto:mfmafkar@gmail.com">
+          <EmailIcon />
+        </IconButton>
       </Box>
     </Box>
   );
 };
+
 export default SocialLinks;
